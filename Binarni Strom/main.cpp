734a6b3983a580f36294e7de9c1011a255a45b44
Adam Zikmund <funn3rcz@gmail.com>
2013-05-02 13:36:03 +0200

    2
    nefunguje search

--- Binarni Strom/main.cpp
+++ Binarni Strom/main.cpp
@@ -11,26 +11,32 @@ int main()
 	testNode->leftLeaf = NULL;
 	testNode->rightLeaf = NULL;
 
-	binTree *objekt = new binTree(testNode);
-
 	node *testNode2 = new node;
-	testNode->value = 12;
-	testNode->leftLeaf = NULL;
-	testNode->rightLeaf = NULL;
+	testNode2->value = 12;
+	testNode2->leftLeaf = NULL;
+	testNode2->rightLeaf = NULL;
 
 	node *testNode3 = new node;
-	testNode->value = 8;
-	testNode->leftLeaf = NULL;
-	testNode->rightLeaf = NULL;
+	testNode3->value = 8;
+	testNode3->leftLeaf = NULL;
+	testNode3->rightLeaf = NULL;
 
-	objekt->addNewLeaf(testNode2);
-	objekt->addNewLeaf(testNode3);
+	binTree *tree = new binTree(testNode);
 
-	node *hmm = objekt->getMainLeaf();
+	// tree->addNewLeaf(testNode2);
+	// tree->addNewLeaf(testNode3);
 
-	cout << objekt->getMainLeaf()->value << endl;
+	node *hmm = tree->getMainLeaf();
+	hmm->leftLeaf = testNode3;
+	hmm->rightLeaf = testNode2;
+
+	cout << hmm->value << endl;
 	cout << hmm->leftLeaf->value << endl;
 	cout << hmm->rightLeaf->value << endl;
+
+	node *hmm2 = tree->searchLeaf(12);
+
+	cout << hmm2->value << endl;
 
 	return 0;
 }
